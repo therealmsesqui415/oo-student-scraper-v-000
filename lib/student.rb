16594f3f96afff94ab8a1016001eb120825b8dbf
Luther Meadows <therealmsesqui@gmail.com>
2017-10-09 06:59:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   def initialize(student_hash)
     student_hash.each do |key, value|
-      self.send("#{attribute}=", value)
+      self.send("#{key}=", value)
     end
     @@all << self
   end
